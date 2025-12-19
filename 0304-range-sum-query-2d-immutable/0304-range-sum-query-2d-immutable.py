62df1f8286e768700684c307e81224364f9a232b
Xiaoha <156593315+xiaoha-cloud@users.noreply.github.com>
2025-12-19 17:22:16 +0000

    Time: 111 ms (59.78%), Space: 32.2 MB (5.2%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,29 +1,26 @@
 class NumMatrix:
 
-    def __init__(self, matrix):
-        m, n = len(matrix), len(matrix[0])
-        self.pre = [[0] * (n + 1) for _ in range(m + 1)]
+    def __init__(self, matrix: List[List[int]]):
+        ROWS,COLS =len(matrix),len(matrix[0])
+        self.sumMat = [ [0] * (COLS +1) for  _ in range(ROWS+1)]
 
-        for r in range(1, m + 1):
-            row_sum = 0
-            for c in range(1, n + 1):
-                row_sum += matrix[r - 1][c - 1]
-                self.pre[r][c] = self.pre[r - 1][c] + row_sum
+        for r in range(ROWS):
+            prefix = 0
+            for c in range(COLS):
+                prefix +=matrix[r][c]
+                above = self.sumMat[r][c+1]
+                self.sumMat[r+1][c+1] = prefix + above
 
-    def sumRegion(self, row1, col1, row2, col2):
-        r1, c1, r2, c2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
-        return (self.pre[r2][c2]
-                - self.pre[r1 - 1][c2]
-                - self.pre[r2][c1 - 1]
-                + self.pre[r1 - 1][c1 - 1])
-
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        row1,col1,row2,col2 = row1+1,col1+1,row2+1,col2+1
+        bottomRight = self.sumMat[row2][col2]
+        above = self.sumMat[row1-1][col2]
+        left = self.sumMat[row2][col1-1]
+        topLeft = self.sumMat[row1-1][col1-1]
+        return bottomRight - above - left + topLeft
+        
 
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
-
-# 0  1  2  3  4 
-# 3  3  4  8  10
-# 15 21 24 26 27 
-
